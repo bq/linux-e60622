eaaba3a007f3ba6f171df203851dcbabb0fb1d3a
Heiko Stuebner <heiko@sntech.de>
2012-11-01 15:04:46 +0100

    zforce: basic coord report including multitouch properties

--- drivers/input/touchscreen/zforce_ts.c
+++ drivers/input/touchscreen/zforce_ts.c
@@ -80,23 +80,25 @@
 #define NOTIFICATION_PROXIMITY		0x26
 #define NOTIFICATION_INVALID_COMMAND	0xfe
 
-
-
-struct touch_info_data_t {
-	u16	x;
-	u16	y;
-	u8	id;
-	u8	state;
-	u8 prblty;
-	u8 valid;
-	u8 rsvrd;
-	u16 z;
-};
-
 #define ZFORCE_REPORT_POINTS 2
 
-// Platform specific
-#define ZF_COORDATA_SIZE 7
+#define STATE_DOWN 0
+#define STATE_MOVE 1
+#define STATE_UP   2
+
+#define SETCONFIG_DUALTOUCH (1 << 0)
+
+struct zforce_point {
+	int coord_x;
+	int coord_y;
+	int state;
+	int id;
+	int area_major;
+	int area_minor;
+	int orientation;
+	int pressure;
+	int prblty;
+};
 
 struct zforce_ts {
 	struct i2c_client	*client;
@@ -120,9 +122,6 @@ struct zforce_ts {
 	u8	zf_status_info[64];
 	int	err_cnt;
 };
-
-static u8  tframe_size =0;
-static u8 reported_finger_count = 0;
 
 static int zforce_command(struct zforce_ts *ts, u8 cmd)
 {
@@ -230,41 +229,18 @@ static int zforce_scan_frequency(struct zforce_ts *ts, u16 idle, u16 finger, u16
 	return zforce_send_wait(ts, &buf[0], ARRAY_SIZE(buf));
 }
 
-//////////////////////////////// todo ////////////////////////////////
-
-// SETCONFIGURATION Request
-// [1:cmd] [2:width] [2:height]
-// ############################
-static int send_setconfig(struct zforce_ts *ts, u32 setconfig)
+static int zforce_setconfig(struct zforce_ts *ts, char b1)
 {
-	struct i2c_msg msg[2];
-	u8 request[16];
-	int ret;
+	struct i2c_client *client = ts->client;
+	char buf[7] = { FRAME_START, 5, COMMAND_SETCONFIG,
+			b1, 0, 0, 0 };
 
-	dev_info(&ts->client->dev, "%s(%d)\n", __FUNCTION__, setconfig);
+	dev_dbg(&client->dev, "set config to (%d)\n", b1);
 
-	request[0] = COMMAND_SETCONFIG;
-	memcpy(&request[1], &setconfig, sizeof(u32));
-
-	msg[0].addr = ts->client->addr;
-	msg[0].flags = 0;
-	msg[0].len = 5;
-	msg[0].buf = request;
-
-	ret = i2c_transfer(ts->client->adapter, msg, 1);
-	if (ret < 0)
-	{
-		dev_err(&ts->client->dev, "i2c send setconfig error: %d\n", ret);
-		return ret;
-	}
-
-	if (wait_for_completion_timeout(&ts->command_done, WAIT_TIMEOUT) == 0)
-		return -1;
-
-	// I2C opperations was successful
-	// Return the results from the controler. (0 == success)
-	return ts->command_result;
+	return zforce_send_wait(ts, &buf[0], ARRAY_SIZE(buf));
 }
+
+//////////////////////////////// todo ////////////////////////////////
 
 // Fixed Pulse and Strength Request
 // ############################
@@ -361,93 +337,6 @@ static int process_level_response(struct zforce_ts *ts, u8* payload)
 	return ZF_LEDDATA_LEN;
 }
 
-
-#define STATE_DOWN 0
-#define STATE_MOVE 1
-#define STATE_UP   2
-
-struct touch_info_data_t tinfo[ZFORCE_REPORT_POINTS];
-//
-// Clear touch info control block
-//
-void touchdata_clear(void)
-{
-	u8 i;
-	for( i=0; i< ZFORCE_REPORT_POINTS; i++ )
-	{
-		tinfo[i].x		= 0;
-		tinfo[i].y		= 0;
-		tinfo[i].id		= 0;
-		tinfo[i].state	= 0;
-		tinfo[i].valid	= 0;
-		tinfo[i].rsvrd	= 0;
-		tinfo[i].prblty	= 0;
-		tinfo[i].z		= 0;
-	}
-}
-
-//
-// return the number of touches
-// return error otherwise
-static u32 framecounter = 0;
-int touchdata_collect( u8* payload )
-{
-	u8 i;
-	reported_finger_count = payload[0];
-	framecounter++;
-	if( reported_finger_count > ZFORCE_REPORT_POINTS )
-	{
-		pr_err("Detected (%d) more fingers the max(%d) number supported\n",
-				reported_finger_count, ZFORCE_REPORT_POINTS );
-		return -EINVAL ;
-	}
-
-	for( i=0; i< reported_finger_count; i++ )
-	{
-		tinfo[i].x	= (u16)((payload[2+i*ZF_COORDATA_SIZE]<<8)|
-					payload[1+i*ZF_COORDATA_SIZE]);
-		tinfo[i].y	= (u16)((payload[4+i*ZF_COORDATA_SIZE]<<8)|
-					payload[3+i*ZF_COORDATA_SIZE]);
-		tinfo[i].id	= (u8)((payload[5+i*ZF_COORDATA_SIZE]&0x3C)>>2);
-		tinfo[i].state	= (u8)((payload[5+i*ZF_COORDATA_SIZE]&0xC0)>>6);
-		tinfo[i].rsvrd	= (u8)( payload[6+i*ZF_COORDATA_SIZE] );
-		tinfo[i].prblty	= (u8)( payload[7+i*ZF_COORDATA_SIZE] );
-		tinfo[i].valid	= 1;
-		tinfo[i].z = reported_finger_count == 0 ? 0 : 20;
-	}
-	return reported_finger_count;
-}
-
-//
-// display touch data buffer
-//
-#ifdef ZF_USE_DEBUG
-void touchdata_show(struct zforce_ts *ts)
-{
-	u8 i;
-
-	if (ts->version_major > 1)
-	{
-		int i;
-		printk("NumFingers=%02d\n", reported_finger_count);
-		for(i=0; i<reported_finger_count; i++)
-		{
-			printk("[%05d](%03d, %03d, %02d) DMU%d ID%d R%02X P%02X V%02d\n",
-				framecounter,
-				tinfo[i].x,
-				tinfo[i].y,
-				tinfo[i].z,
-				tinfo[i].state,
-				tinfo[i].id,
-				tinfo[i].rsvrd,
-				tinfo[i].prblty,
-				tinfo[i].valid );
-		}
-		printk("\n");
-	}
-}
-#endif
-
 // Fix Pulse Strength  Payload Results
 // [1:x] [1:y] [3*x:xdata] [3*y:ydata]
 // #####################################
@@ -485,103 +374,84 @@ static int process_pulsestreng_response(struct zforce_ts *ts, u8* payload)
 // Touch Payload Results
 // [1:count] [2:x] [2:y] [1:state]
 // ###############################
-static int process_touch_event(struct zforce_ts *ts, u8* payload)
+static int zforce_touch_event(struct zforce_ts *ts, u8* payload)
 {
-	u16 x,y;
-	u8  status;
-	int count;
-	u8 id = 0;
-	u8 state = 0;
-	int size = 0;
+	struct i2c_client *client = ts->client;
+	struct zforce_point point[ZFORCE_REPORT_POINTS];
+	const struct zforce_ts_platdata *pdata = client->dev.platform_data;
+	int count, i;
 
-	// Request the next event ASAP.
-	if (ts->version_major == 1)
-	{
-		size = 5;
-	}
-	else
-	{
-		size = 7;
-	}
-
-#ifdef ZF_USE_DEBUG
-	// =-=-=-=-=-=-=-=-=-=
-	//  Get touch data
-	// =-=-=-=-=-=-=-=-=-=
-	retval = touchdata_collect( payload ) ;
-	if( retval < 0 )
-		return retval;
-
-	touchdata_show();
-#endif
 
 	count = payload[0];
-	#define ZF_COORDATA_SIZE 7
-	#ifdef ZF_USE_DEBUG
-	if (ts->version_major > 1)
-	{
-		int i;
-		printk("NumFingers=%02d\n", count);
-		for(i=0; i<count; i++)
-			printk("(%03d, %03d) %d %d %02X %02X ",
-				(int)((payload[2+i*ZF_COORDATA_SIZE]<<8)|payload[1+i*ZF_COORDATA_SIZE]),
-				(int)((payload[4+i*ZF_COORDATA_SIZE]<<8)|payload[3+i*ZF_COORDATA_SIZE]),
-				(int)((payload[5+i*ZF_COORDATA_SIZE]&0xC0)>>6),
-				(int)((payload[5+i*ZF_COORDATA_SIZE]&0x3C)>>2),
-				payload[6+i*ZF_COORDATA_SIZE],
-				payload[7+i*ZF_COORDATA_SIZE] );
-		printk("\n");
-	}
-	#endif
-	if (count != 1)
-	{
-		dev_dbg(&ts->client->dev, "Invalid number of coordinates: %d\n", count);
-	}
-	memcpy(&x, &payload[1], sizeof(u16));
-	memcpy(&y, &payload[3], sizeof(u16));
-	status = payload[5];
-
-	if (ts->version_major == 1)
-	{
-		state = status & 0x03;
-		id = 1;
-	}
-	else
-	{
-		state = (status & 0xC0) >> 6;
-		id =    (status & 0x3C) >> 2;
+	if (count > ZFORCE_REPORT_POINTS) {
+		dev_warn(&client->dev, "to many coordinates %d\n", count);
+		count = ZFORCE_REPORT_POINTS;
 	}
 
-	//x = 600 - x;
-	if (ts->version_major == 1)
-		y = 800 - y;
+	for (i = 0; i < count; i++) {
+		point[i].coord_x =
+			payload[9 * i + 2] << 8 | payload[9 * i + 1];
+		point[i].coord_y =
+			payload[9 * i + 4] << 8 | payload[9 * i + 3];
 
-	switch (state) {
-	/* fall-through, input subsystem handles an unchanged BTN_TOUCH
-	 * property itself
-	 */
-	case STATE_MOVE:
-		dev_dbg(&ts->client->dev, "%d move(%d,%d)\n", id, x, y);
-	case STATE_DOWN:
-		dev_dbg(&ts->client->dev, "%d down(%d,%d)\n", id, x, y);
-		input_report_abs(ts->input, ABS_X, x);
-		input_report_abs(ts->input, ABS_Y, y);
+		if (point[i].coord_x > pdata->x_max ||
+		    point[i].coord_y > pdata->y_max) {
+			dev_warn(&client->dev, "coordinates (%d,%d) invalid\n",
+				point[i].coord_x, point[i].coord_y);
+			point[i].coord_x = point[i].coord_y = 0;
+		}
+
+		point[i].state = payload[9 * i + 5] & 0x03;
+		point[i].id = (payload[9 * i + 5] & 0xfc) >> 2;
+
+		/* determine touch major, minor and orientation */
+		point[i].area_major = max(payload[9 * i + 6], payload[9 * i + 7]);
+		point[i].area_minor = min(payload[9 * i + 6], payload[9 * i + 7]);
+		point[i].orientation = payload[9 * i + 6] > payload[9 * i + 7];
+		
+		point[i].pressure = payload[9 * i + 8];
+		point[i].prblty = payload[9 * i + 9];
+	}
+
+
+	for (i = 0; i < count; i++) {
+		dev_dbg(&client->dev, "touch-data for point %d of %d: state %d, id %d, pressure %d, probability %d\n",
+			i, count, point[i].state, point[i].id,
+			point[i].pressure, point[i].prblty);
+		dev_dbg(&client->dev, "x %d, y %d, area-major %d, area_minor %d, orientation %d\n",
+			point[i].coord_x, point[i].coord_y,
+			point[i].area_major, point[i].area_minor,
+			point[i].orientation);
+
+		if (point[i].state == STATE_DOWN || point[i].state == STATE_MOVE) {
+			input_report_abs(ts->input, ABS_MT_POSITION_X,
+					 point[i].coord_x);
+			input_report_abs(ts->input, ABS_MT_POSITION_Y,
+					 point[i].coord_y);
+			input_report_abs(ts->input, ABS_MT_TOUCH_MAJOR,
+					 point[i].area_major);
+			input_report_abs(ts->input, ABS_MT_TOUCH_MINOR,
+					 point[i].area_minor);
+			input_report_abs(ts->input, ABS_MT_ORIENTATION,
+					 point[i].orientation);
+			input_mt_sync(ts->input);
+		}
+	}
+
+
+	input_report_abs(ts->input, ABS_X, point[0].coord_x);
+	input_report_abs(ts->input, ABS_Y, point[0].coord_y);
+	if (point[0].state == STATE_DOWN || point[0].state == STATE_MOVE) {
 		input_report_abs(ts->input, ABS_PRESSURE, 1024); /* FIXME: not for upstream, but for old tslib versions */
 		input_report_key(ts->input, BTN_TOUCH, 1);
-		break;
-	case STATE_UP:
-		dev_dbg(&ts->client->dev, "%d up(%d,%d)\n", id, x, y);
-		input_report_abs(ts->input, ABS_X, x);
-		input_report_abs(ts->input, ABS_Y, y);
+	} else {
 		input_report_abs(ts->input, ABS_PRESSURE, 0); /* FIXME: not for upstream, but for old tslib versions */
 		input_report_key(ts->input, BTN_TOUCH, 0);
-		break;
-	default:
-		dev_err(&ts->client->dev, "Invalid state: %d\n", state);
-		return (count * size) + 1;
 	}
 	input_sync(ts->input);
-	return (count * size) + 1;
+
+
+	return 0;
 }
 
 static int zforce_read_packet(struct zforce_ts *ts, u8 *buffer)
@@ -640,10 +510,12 @@ static irqreturn_t zforce_interrupt(int irq, void *dev_id)
 	u8 payload_buffer[512];
 	u8 *payload;
 
-dev_err(&client->dev, "intstart, gpio: %d, value: %d\n", pdata->gpio_int, gpio_get_value(pdata->gpio_int));
+//	pm_stay_awake(&client->dev);
+
+//dev_err(&client->dev, "intstart, gpio: %d, value: %d\n", pdata->gpio_int, gpio_get_value(pdata->gpio_int));
 
 	while(!gpio_get_value(pdata->gpio_int)) {
-dev_err(&client->dev, "inthandler, gpio: %d, value: %d\n", pdata->gpio_int, gpio_get_value(pdata->gpio_int));
+//dev_err(&client->dev, "inthandler, gpio: %d, value: %d\n", pdata->gpio_int, gpio_get_value(pdata->gpio_int));
 
 		ret = zforce_read_packet(ts, payload_buffer);
 		if (ret < 0) {
@@ -653,14 +525,14 @@ dev_err(&client->dev, "inthandler, gpio: %d, value: %d\n", pdata->gpio_int, gpio
 
 		payload =  &payload_buffer[PAYLOAD_BODY];
 
-		dev_err(&ts->client->dev, " response frame:  " );
+/*		dev_err(&ts->client->dev, " response frame:  " );
 		for (i = 0; i < (payload_buffer[PAYLOAD_LENGTH] + 2); i++ )
 			printk( " 0x%02X ", payload_buffer[i] );
-		printk( "\n" );
+		printk( "\n" );*/
 
 		switch (payload[RESPONSE_ID]) {
 		case NOTIFICATION_TOUCH:
-			process_touch_event(ts, &payload[RESPONSE_DATA]);
+			zforce_touch_event(ts, &payload[RESPONSE_DATA]);
 			break;
 		case NOTIFICATION_BOOTCOMPLETE:
 			ts->boot_complete = payload[RESPONSE_DATA];
@@ -692,11 +564,6 @@ dev_err(&client->dev, "inthandler, gpio: %d, value: %d\n", pdata->gpio_int, gpio
 			memcpy(&ts->version_minor, &payload[RESPONSE_DATA+2], sizeof(u16));
 			memcpy(&ts->version_build, &payload[RESPONSE_DATA+4], sizeof(u16));
 			memcpy(&ts->version_rev,   &payload[RESPONSE_DATA+6], sizeof(u16));
-	// Request the next event ASAP.
-	if (ts->version_major == 1)
-		tframe_size = 5;
-	else
-		tframe_size = 7;
 	dev_info(&ts->client->dev, "Firmware Version %04x:%04x %04x:%04x\n", ts->version_major, ts->version_minor, ts->version_build, ts->version_rev);
 
 			u8 *pyld =  NULL;
@@ -714,14 +581,16 @@ dev_err(&client->dev, "inthandler, gpio: %d, value: %d\n", pdata->gpio_int, gpio
 			break;
 		default:
 			dev_err(&ts->client->dev, "unrecognized response id: 0x%x (err_cnt: %d)\n", payload[RESPONSE_ID], ++(ts->err_cnt) );
-			dev_err(&ts->client->dev, " response frame:  " );
+/*			dev_err(&ts->client->dev, " response frame:  " );
 			for (i = 0; i < (payload_buffer[PAYLOAD_LENGTH] + 2); i++ )
 				printk( " 0x%02X ", payload_buffer[i] );
-			printk( "\n" );
+			printk( "\n" );*/
 			break;
 		}
 	}
-dev_err(&client->dev, "intend, gpio: %d, value: %d\n", pdata->gpio_int, gpio_get_value(pdata->gpio_int));
+//dev_err(&client->dev, "intend, gpio: %d, value: %d\n", pdata->gpio_int, gpio_get_value(pdata->gpio_int));
+
+//	pm_relax();
 
 	return IRQ_HANDLED;
 }
@@ -758,18 +627,13 @@ static int zforce_start(struct zforce_ts *ts)
 		goto error;
 	}
 
-/*
-	#define ZF_SETCONFIG_DUALTOUCH 0x00000001
-	// Set configuration, enable dual touch
-	if (send_setconfig(ts, ZF_SETCONFIG_DUALTOUCH))
-	{
+	/* enable dual touch */
+	if (zforce_setconfig(ts, SETCONFIG_DUALTOUCH)) {
 		dev_err(&client->dev, "Unable to set config\n");
-		goto err_free_irq;
+		goto error;
 	}
 
-*/
-
-	// This will start sending touch events.
+	/* start sending touch events */
 	ret = zforce_command(ts, COMMAND_DATAREQUEST);
 	if (ret) {
 		dev_err(&client->dev, "Unable to request data\n");
