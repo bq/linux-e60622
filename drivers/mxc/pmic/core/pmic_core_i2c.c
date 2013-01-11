5004d8f59721e2dc1eedea6c51913e59b55492fd
Heiko Stuebner <heiko@sntech.de>
2013-01-11 10:47:48 +0100

    Revert "block i2c operations to the msp when the power button is pressed"
    This reverts commit 9d0a9d7cf216fa5df8e198227b3f6d2691c01e50.

--- drivers/mxc/pmic/core/pmic_core_i2c.c
+++ drivers/mxc/pmic/core/pmic_core_i2c.c
@@ -38,7 +38,6 @@
 #include <linux/delay.h>
 #include <linux/uaccess.h>
 #include <mach/hardware.h>
-#include <linux/gpio.h>
 
 #include "pmic.h"
 
@@ -432,8 +431,6 @@ static void add_crc16(unsigned char *data, int len) {
 
 }
 
-#define GPIO_PWR_SW		(3*32 + 10)	/*GPIO_4_10 */
-
 static int msp430_cmd(int cmd, int arg, u8 *indata, int nwrite, u8 *outdata, int nread, int crc) {
 
 	struct i2c_client *client = msp430_client;
@@ -441,16 +438,6 @@ static int msp430_cmd(int cmd, int arg, u8 *indata, int nwrite, u8 *outdata, int
 	u8 buffer[32];
 	u16 addr = client->addr;
 	u16 flags = client->flags;
-	int pwr_key;
-
-	if ((6 == check_hardware_name()) || (2 == check_hardware_name())) 		// E60632 || E50602
-		pwr_key = gpio_get_value (GPIO_PWR_SW)?1:0;
-	else
-		pwr_key = gpio_get_value (GPIO_PWR_SW)?0:1;
-	if (pwr_key) {
-		printk(KERN_INFO "msp430_cmd blocked by pressed power key\n");
-		return -EINVAL;
-	}
 
 	struct i2c_msg msg[2] = {
 		{addr, flags, 2+nwrite, buffer},
@@ -497,7 +484,6 @@ unsigned int msp430_read(unsigned int reg)
 	int i2c_ret;
 	int retry_count = 5;
 	int value = -1;
-	int pwr_key;
 	u8 buf0[2], buf1[2]={0,0};
 	u16 addr;
 	u16 flags;
@@ -509,15 +495,6 @@ unsigned int msp430_read(unsigned int reg)
 	if (!msp430_client) {
 		printk("[%s-%d] MSP430 not probed...\n",__FUNCTION__,__LINE__);
 		return 0;
-	}
-
-	if ((6 == check_hardware_name()) || (2 == check_hardware_name())) 		// E60632 || E50602
-		pwr_key = gpio_get_value (GPIO_PWR_SW)?1:0;
-	else
-		pwr_key = gpio_get_value (GPIO_PWR_SW)?0:1;
-	if (pwr_key) {
-		printk(KERN_INFO "msp430_read blocked by pressed power key\n");
-		return -EINVAL;
 	}
 	
 	if ((0x60 == reg) && (NEWMSP))
@@ -550,23 +527,12 @@ int msp430_write(unsigned int reg, unsigned int value)
 	u16 flags;
 	u8 buf[4];
 	int i2c_ret;
-	int pwr_key;
 	struct i2c_msg msg = { 0, 0, 3, buf };
 
 	if (!msp430_client) {
 		printk("[%s-%d] MSP430 not probed...\n",__FUNCTION__,__LINE__);
 		return 0;
 	}
-
-	if ((6 == check_hardware_name()) || (2 == check_hardware_name())) 		// E60632 || E50602
-		pwr_key = gpio_get_value (GPIO_PWR_SW)?1:0;
-	else
-		pwr_key = gpio_get_value (GPIO_PWR_SW)?0:1;
-	if (pwr_key) {
-		printk(KERN_INFO "msp430_read blocked by pressed power key\n");
-		return -EINVAL;
-	}
-
 	msg.addr = client->addr;
 	msg.flags = client->flags;
 	
